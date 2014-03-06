e3c80796630248703a198e48b83b7f9c127c2043
underchemist <yanns.tremblay@gmail.com>
2014-03-06 14:30:41 -0500

    Thu Mar  6 14:30:41 EST 2014

--- prob34/prob34.cpp
+++ prob34/prob34.cpp
@@ -6,14 +6,33 @@
 using namespace std;
 using namespace boost::math;
 
-string numToString(int n) 
+double fact(int n) 
 {
-    string str = to_string(factorial<float>(n));
-    return str;
+    return factorial<double>(n);
 }
 
 int main() 
 {
-    cout << numToString(4) << endl;
-	return 0;
+    auto start = chrono::high_resolution_clock::now();
+    int sum = 0;
+    for (int i = 10; i < 999999; i++)
+    {
+        string str = to_string(i);
+        int temp_sum = 0;
+        for (auto it = str.begin(); it != str.end(); ++it)
+        {  
+            int temp_digit = *it - '0'; // there should be a better way to do this ...
+            temp_sum += int(fact(temp_digit));
+        }
+        if (temp_sum == i)
+        {
+            sum += i;
+        }
+    }
+
+    cout << "sum of all numbers which are equal to factorial digits: " << sum << endl;
+    auto end = chrono::high_resolution_clock::now();
+    cout << "time: " << chrono::duration_cast<chrono::milliseconds>(end-start).count() << " ms" << endl;
+	
+    return 0;
 }
